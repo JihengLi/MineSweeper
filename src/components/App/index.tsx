a9b32516c9aa8f7b68ae2ca553fdd3a5c19b1257
JihengLi <jiheng.li.1@vanderbilt.edu>
2024-02-19 00:56:57 -0600

    build the UI of header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 import "./App.scss";
+import NumberDisplay from "../NumberDisplay";
 
 const App = () => {
-  return <div className="App">Hello from App;</div>;
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="image" aria-label="face">
+            🙂
+          </span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
+    </div>
+  );
 };
 
 export default App;
