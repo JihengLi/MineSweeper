1af04ce2158932216b02963ec3421f45eddad438
JihengLi <jiheng.li.1@vanderbilt.edu>
2024-03-10 12:25:09 -0500

    write the internal game logic #1

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,9 +8,18 @@ interface ButtonProps {
   col: number;
   state: CellState;
   value: CellValue;
+  onClick(rowParam: number, colParam: number): (...args: any[]) => void;
+  onContext(rowParam: number, colParam: number): (...args: any[]) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ state, value, row, col }) => {
+const Button: React.FC<ButtonProps> = ({
+  state,
+  value,
+  onContext,
+  onClick,
+  row,
+  col,
+}) => {
   const renderContent = (): React.ReactNode => {
     if (state === CellState.visible) {
       if (value === CellValue.bomb) {
@@ -38,6 +47,8 @@ const Button: React.FC<ButtonProps> = ({ state, value, row, col }) => {
   return (
     <div
       className={`Button ${state === CellState.visible ? "visible" : ""} value-${value}`}
+      onClick={onClick(row, col)}
+      onContextMenu={onContext(row, col)}
     >
       {renderContent()}
     </div>
